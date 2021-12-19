7c08f316d99cc7e1ae393335c4ac0c5e7f1114d9
dmitriydubodelov <gradient27@mail.ru>
2021-12-19 18:07:31 +0300

    Создан файл Задание 3 (занятие 1).py

---
+++ Задание 3 (занятие 1).py
@@ -0,0 +1,7 @@
+a = input('Введите число: ')
+summand_1 = int(a)
+summand_2 = int(a + a)
+summand_3 = int(a + a + a)
+summ = summand_1 + summand_2 + summand_3
+
+print(f'Результат странных вычислений равен: {summ}')
