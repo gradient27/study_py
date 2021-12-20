c93aeb43fa2d6771ea7bc9a7c80456cb9d6c120f
dmitriydubodelov <gradient27@mail.ru>
2021-12-20 14:25:16 +0300

    Внес изменения в соответсвии с разбором

--- Задание 4 (занятие 1).py
+++ Задание 4 (занятие 1).py
@@ -1,4 +1,5 @@
-# -------------Решение через перебор символов строки------
+# -------------Решение мое через перебор символов строки------
+'''
 a_str = (input('Введите число: '))
 cnt_num = len(a_str)
 # i - счетчик для символов строки
@@ -10,7 +11,7 @@ while j >= 0:
     while i < cnt_num:
         # print(f'Начало проверки, i = {i}, j = {j}')
         if int(a_str[i]) == j:
-            #print (a_str[i], j)
+            #print(a_str[i], j)
             result = j
             break
         else:
@@ -21,3 +22,21 @@ while j >= 0:
         j = j - 1
 
 print(f'Наибольшее число равно: {result}')
+'''
+# ----Конец моего решения
+
+# ----Решение Евгения
+num_init = int(input('Введите целое положительное число: '))
+maximum = num_init % 10
+num = num_init
+while num > 0:
+    digit = num % 10
+    if digit > maximum:
+        maximum = digit
+
+    if digit == 9:
+        break
+
+    num //= 10 # num = num // 10
+    print(num)
+print(f'Наибольшая цифра в числе {num_init} равна {maximum}')
