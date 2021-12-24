da15911977ff7e3dedb3be1bcc5e6734db47aea0
dmitriydubodelov <gradient27@mail.ru>
2021-12-24 19:21:14 +0300

    Пора оставить задание 1 в покое

--- Задание 1 (занятие 3).py
+++ Задание 1 (занятие 3).py
@@ -1,20 +1,13 @@
 # Решение 1, нет проверки на тип введенных данных и при нуле нет повтора ввода
+'''
 def my_devision(delimoe, delitel):
-    ''' Функиция вычисляет результат деления'''
     return delimoe / delitel
 
 def input_elem():
     delimoe = int(input('Введите делимое: '))
     delitel = int(input('Введите делитель: '))
     return delimoe, delitel
-'''
-def validation(delimoe, delitel):
-    delimoe, delitel = input_elem()
-    if delitel != 0:
-        return delimoe, delitel
-    else:
-        print('Значение вычислить не возможно, делитель равено 0')
-'''
+
 def main():
     delimoe, delitel = input_elem()
     if delitel != 0:
@@ -22,5 +15,22 @@ def main():
     else:
         print('Значение вычислить не возможно, делитель равено 0')
 
+main()
+'''
+# Решение 2, если 0 предлагается ввести делитель повторно
+def input_elem():
+    delitel = 0
+    delimoe = int(input('Введите делимое: '))
+    while delitel == 0:
+        delitel = int(input('Введите делитель (число не равное 0): '))
+    return delimoe, delitel
+
+def my_devision(delimoe, delitel):
+    ''' Функиция вычисляет результат деления'''
+    return delimoe / delitel
+
+def main():
+    delimoe, delitel = input_elem()
+    print(f'Результат деления: {my_devision(delimoe, delitel)}')
 
 main()
