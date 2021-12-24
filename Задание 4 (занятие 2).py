e26d39eb2aec97f24152a02f5e770585b99faa45
dmitriydubodelov <gradient27@mail.ru>
2021-12-24 08:29:45 +0300

    Разобраны решения

--- Задание 4 (занятие 2).py
+++ Задание 4 (занятие 2).py
@@ -1,3 +1,5 @@
+# --- Мое решение ---
+'''
 my_str = input('Введите несколько слов, разделенных пробелами: ')
 print(f'Вы ввели: {my_str}')
 my_list = my_str.split(' ')
@@ -6,3 +8,10 @@ cnt = 1
 for el in my_list:
     print(f'{cnt}. {el:.10}')
     cnt += 1
+# --- Конец моего решения ---
+'''
+
+# --- Решение Евгения ---
+string = input('Введите слова через пробелы: ').split()
+for i, word in enumerate(string, 1):
+    print(f'{i}, {word[:10]}')
--- Задание 5 (занятие 2).py
+++ Задание 5 (занятие 2).py
@@ -1,4 +1,14 @@
-my_list = [7, 5, 3, 3, 2]
-new_int = int(input(f'Введите новое значение рейтинга: '))
-ind = my_list.index(new_int)
-print(ind)
+# --- Решение Евгения --
+my_l = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+new_number = int(input("Введите новый элемент рейтинга в виде натурального числа: "))
+i = 0
+
+for n in my_l:
+    if new_number <= n:
+        i += 1
+
+    elif new_number > n:
+        break
+
+my_l.insert(i, float(new_number))
+print(my_l)
