9bf58b6b1f4b705b833cbd974db795073ebc6c07
dmitriydubodelov <gradient27@mail.ru>
2021-12-24 18:07:45 +0300

    Задине 1 без проверки 0

--- Задание 1 (занятие 3).py
+++ Задание 1 (занятие 3).py
@@ -1,6 +1,16 @@
-def my_devision (delimoe, delitel):
+def my_devision(delimoe, delitel):
     ''' Функиция вычисляет результат деления'''
     return delimoe / delitel
 
-def input_elem ():
+def input_elem():
     '''Функция запрашивает делимое и делитель у пользователя, проверяет полученные значения'''
+    delimoe = int(input('Введите делимое: '))
+    delitel = int(input('Введите делитель: '))
+    return delimoe, delitel
+
+
+def main():
+    delimoe, delitel = input_elem()
+    print(f'Результат деления: {my_devision(delimoe, delitel)}')
+
+main()
