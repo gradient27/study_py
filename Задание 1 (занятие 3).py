dfe14191b9671625be8e990067e252db0723a4a0
dmitriydubodelov <gradient27@mail.ru>
2021-12-24 18:55:10 +0300

    Задание 1 (решение мне не очень нравится)

--- Задание 1 (занятие 3).py
+++ Задание 1 (занятие 3).py
@@ -7,14 +7,14 @@ def input_elem():
     delimoe = int(input('Введите делимое: '))
     delitel = int(input('Введите делитель: '))
     return delimoe, delitel
-
+'''
 def validation(delimoe, delitel):
     delimoe, delitel = input_elem()
     if delitel != 0:
         return delimoe, delitel
     else:
         print('Значение вычислить не возможно, делитель равено 0')
-
+'''
 def main():
     delimoe, delitel = input_elem()
     if delitel != 0:
