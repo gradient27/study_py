430733546054b17e27a524b6134a49fae5674356
dmitriydubodelov <gradient27@mail.ru>
2021-12-25 23:04:46 +0300

    Завершил работу над заданием 6 (зянятие 3)

--- Задание 6 (занятие 3).py
+++ Задание 6 (занятие 3).py
@@ -1,9 +1,11 @@
 def my_input():
-    my_str = input('Введите слово: ')
+    my_str = input('Введите слова через пробел: ')
     return my_str
 
 def int_func():
     my_str = my_input()
-    print(my_str)
+    new_str = my_str.title()
+
+    print(new_str)
 
 int_func()
