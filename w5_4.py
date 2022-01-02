7a9015085681ef7632674ca7efa1a328aec3e06b
dmitriydubodelov <gradient27@mail.ru>
2022-01-02 12:32:07 +0300

    Допилил Задание 5 к занятию 4

--- w5_4.py
+++ w5_4.py
@@ -1,3 +1,2 @@
 from functools import reduce
-my_list = [el for el in range(100, 1001) if el % 2 == 0]
-print(reduce(lambda a, b: a * b, my_list))
+print(reduce(lambda a, b: a * b, [el for el in range(100, 1001) if el % 2 == 0]))
