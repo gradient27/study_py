aa019849202f6dd8a180055a0818ce06a691736e
dmitriydubodelov <gradient27@mail.ru>
2022-01-06 11:32:49 +0300

    Готов w1_5

--- w1_5.py
+++ w1_5.py
@@ -1,2 +1,13 @@
-with open('data.txt', 'w') as f:
-    f.write('string')
+def rec(str):
+    with open('data.txt', 'a') as f:
+        f.write(f'\nnew string: {str}')
+    print(f'Выполнена запись в файл data.txt строки: {str}')
+
+def my_inp():
+    str = input('Введите текст (для выхода нажмите Enter): ')
+    if len(str) != 0:
+        rec(str)
+    return str
+
+while len(my_inp()) != 0:
+    my_inp()
