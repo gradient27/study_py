8b9e5e5465ec6e82debe5206cb6c0ee71d95329a
dmitriydubodelov <gradient27@mail.ru>
2022-01-08 19:58:12 +0300

    Закончил работу над w6_5

---
+++ w6_5.py
@@ -0,0 +1,23 @@
+with open('data.txt', 'r', encoding='utf-8') as f:
+    my_dict = {}
+    new_dict = {}
+    for line in f:
+        my_list = line.split(":")
+        my_name = my_list[0]
+        switch = False
+        new_list = []
+        sum = 0
+        for el in my_list[1]:
+            if el.isdigit() and switch == False:
+                    new_list.append(el)
+                    switch = True
+            elif el.isdigit() and switch == True:
+                    new_list[len(new_list) - 1] = new_list[len(new_list) - 1] + el
+            else:
+                    switch = False
+        for el in new_list:
+            el = int(el)
+            sum = sum + el
+        new_dict = {my_name : sum}
+        my_dict.update(new_dict)
+    print(my_dict)
