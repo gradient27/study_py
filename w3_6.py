8f961453c1500f164306eb416c399d7f5d9dbdec
dmitriydubodelov <gradient27@mail.ru>
2022-01-17 17:09:28 +0300

    Начал w3_6

---
+++ w3_6.py
@@ -0,0 +1,11 @@
+class Worker():
+
+    def __init__(self, name, surname, position, income):
+        my_dict = {"wade": 10, "bonus": 5}
+        name = self.name
+        surname = self.surnname
+        position = self.position
+        income = my_dict("")
+
+class Position(Worker):
+    pass
