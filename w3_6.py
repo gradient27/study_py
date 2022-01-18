e43346e0e5af757cd1c02be225f2952b1f8a46d8
dmitriydubodelov <gradient27@mail.ru>
2022-01-18 10:30:55 +0300

    Завершил w3_6

--- w3_6.py
+++ w3_6.py
@@ -1,11 +1,23 @@
 class Worker():
 
-    def __init__(self, name, surname, position, income):
-        my_dict = {"wade": 10, "bonus": 5}
-        name = self.name
-        surname = self.surnname
-        position = self.position
-        income = my_dict("")
+    def __init__(self, name, surname, position, wade, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self.income = {'wade': wade, 'bonus': bonus}
 
 class Position(Worker):
-    pass
+
+    def get_full_name(self):
+        print(f'Полное имя объекта: {self.name + " " + self.surname}')
+
+    def get_total_income(self):
+        print(f'Доход объекта: {self.income.get("bonus") + self.income.get("wade")} рублей')
+
+
+kuza = Position('Кузя', 'Кузькин', 'Директор', 1000, 100)
+vova = Position('Вова', 'Вовин', 'Главбух', 700, 50)
+borya = Position('Боря', 'Борин', 'Уборщик', 100, 10)
+
+vova.get_full_name()
+vova.get_total_income()
