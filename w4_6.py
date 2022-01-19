f605b2ce6068a9e0f958e713fa9a93b1fd0ff5c5
dmitriydubodelov <gradient27@mail.ru>
2022-01-19 08:12:22 +0300

    Закончил w4_6

--- w4_6.py
+++ w4_6.py
@@ -13,16 +13,33 @@ class Car():
         print(f'Объект {self} закончил движение')
 
     def turn(self, direction):
-        print(f'Объект {self} поернул в сторону {direction}')
+        print(f'Объект {self} повернул в сторону {direction}')
+
+    def show_speed(self):
+        print(self.speed)
 
 class TownCar(Car):
-    pass
+    def show_speed(self):
+        if self.speed > 60:
+            print('Скорость превышена')
+        else:
+            print(self.speed)
 
 class SportCar(Car):
     pass
 
 class WorkCar(Car):
-    pass
+    def show_speed(self):
+        if self.speed > 40:
+            print('Скорость превышена')
+        else:
+            print(self.speed)
 
 class PoliceCar(Car):
     pass
+
+wk = WorkCar(50, 'black', 'bebeka', False)
+wk.go()
+wk.turn("в левую сторону")
+wk.stop()
+wk.show_speed()
